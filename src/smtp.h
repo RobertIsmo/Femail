cf7b9e75598c450b3b14c99d98aa5cefa58e9287
Robert Ismo <me@robertismo.com>
2026-02-04 11:17:24 -0600

    cleaned up EHLO response

--- src/smtp.h
+++ src/smtp.h
@@ -29,18 +29,21 @@ typedef struct {
 
 #define smtpsmsg_accept_connection()	dprintf(conn->clientsocket, "220 %s ESMTP Femail\r\n", get_mail_domain())
 #define smtpsmsg_accept_hello()			dprintf(conn->clientsocket, "250 Hello %s\r\n", conn->mailclientdomain)
-#define smtpsmsg_accept_ehello()		dprintf(conn->clientsocket, "250-%s Hello %s\n250 SIZE %d\r\n",	\
-												get_mail_domain(),		\
-												conn->mailclientdomain, \
+#define smtpsmsg_accept_ehello()		dprintf(conn->clientsocket, \
+												"250-%s Hello %s\n"	\
+												"250-SIZE %d\n"	\
+												"250 HELP\r\n",	\
+												get_mail_domain(),	\
+												conn->mailclientdomain,	\
 												ESMTP_SIZE)
-#define smtpsmsg_accept_ok()		dprintf(conn->clientsocket, "250 Ok\r\n")
+#define smtpsmsg_accept_ok()			dprintf(conn->clientsocket, "250 Ok\r\n")
 #define smtpsmsg_accept_goodbye()		dprintf(conn->clientsocket, "221 Bye\r\n")
 #define smtpsmsg_start_data()			dprintf(conn->clientsocket, "354\r\n")
 #define smtpsmsg_reject_error()			dprintf(conn->clientsocket, "500 Bye\r\n")
 #define smtpsmsg_reject_sequence()		dprintf(conn->clientsocket, "503 Invalid Sequence\r\n")
 #define smtpsmsg_reject_too_long()		dprintf(conn->clientsocket, "500 Too long\r\n")
 #define smtpsmsg_reject_too_large()		dprintf(conn->clientsocket, "554 5.3.4 Message Too Large\r\n")
-#define smtpsmsg_reject_unrecognized() dprintf(conn->clientsocket, "500 Command Unrecognized\r\n")
+#define smtpsmsg_reject_unrecognized()	dprintf(conn->clientsocket, "500 Command Unrecognized\r\n")
 #define smtpsmsg_reject_unimplemented() dprintf(conn->clientsocket, "502 Command Unimplemented\r\n")
 #define smtpsmsg_reject_starttls()		dprintf(conn->clientsocket, "523 Use SMTPS instead\r\n")
 
