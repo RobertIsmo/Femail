4a9f39c5f944040263fd2a9b20f8632e0ee8d832
Robert Ismo <me@robertismo.com>
2026-02-18 16:21:26 -0600

    Implemented HTTPS

--- src/http.c
+++ src/http.c
@@ -13,7 +13,7 @@ ConnectionHandlerResult http_handler(Connection * conn) {
 		return CONNECTION_DONE;
 	}
 
-	log_debug("Processing a connection\n" \
+	log_debug("Processing a connection\n"		\
 			  "Type: %d\n" \
 			  "State: %d\n" \
 			  "Started: %ld\n" \
@@ -44,10 +44,51 @@ ConnectionHandlerResult http_handler(Connection * conn) {
 
 	log_debug("Received: %s", buffer);
 
-	httpmsg_no_content();
+	httpmsg_redirect_https();
 	return CONNECTION_DONE;
 }
 
-ConnectionHandlerResult https_handler(Connection *) {
-	return CONNECTION_ERROR;
+ConnectionHandlerResult https_handler(Connection * conn) {
+	if (!conn->live) {
+		log_debug("Processing a dead connection. skipping...");
+		return CONNECTION_DONE;
+	}
+
+	log_debug("Processing a connection\n"		\
+			  "Type: %d\n" \
+			  "State: %d\n" \
+			  "Started: %ld\n" \
+			  "Refresh: %ld\n",
+			  conn->type,
+			  conn->state,
+			  conn->timeinitialized,
+			  conn->timerefreshed);
+
+	if (SSL_accept(conn->sslconn) <= 0) {
+		log_debug("Failed TLS negotiation.");
+		return CONNECTION_ERROR;
+	}
+
+	char buffer [SMALL_BUFFER_SIZE] = {0};
+	ssize_t readresult = SSL_read(conn->sslconn,
+						  buffer,
+						  SMALL_BUFFER_SIZE);
+
+	log_debug("read amount: %ld",
+			  readresult);
+
+	if (readresult >= SMALL_BUFFER_SIZE - 1) {
+		return CONNECTION_CONTINUE;
+	} else if (readresult == -1) {
+		if (errno == EWOULDBLOCK) {
+			return CONNECTION_CONTINUE;
+		} else {
+			return CONNECTION_ERROR;
+		}
+	} else if (readresult == 0) {
+		return CONNECTION_ERROR;
+	}
+
+	httpsmsg_no_content();
+	return CONNECTION_DONE;
 }
--- src/http.h
+++ src/http.h
@@ -1,6 +1,24 @@
 #ifndef HTTP_H
 #define HTTP_H
 
-#define httpmsg_no_content() dprintf(conn->clientsocket, "HTTP/1.1 204 No Content\r\n\r\n")
+#define NO_CONTENT_RESPONSE_LENGTH 88
+char nocontentresponse[] = \
+	"HTTP/1.1 204 No Content\r\n"		\
+	"Server: Femail\r\n"				\
+	"Connection: close\r\n"			\
+	"Cache-Control: no-store\r\n"		\
+	"\r\n";
 
+#define httpmsg_no_content() dprintf(conn->clientsocket,	\
+									 nocontentresponse)
+#define httpmsg_redirect_https() dprintf(conn->clientsocket,			\
+										 "HTTP/1.1 301 Moved Permanently\r\n" \
+										 "Server: Femail\r\n"			\
+										 "Location: https://%s/\r\n"	\
+										 "Connection: close\r\n"		\
+										 "\r\n",						\
+										 get_mail_domain())
+#define httpsmsg_no_content() SSL_write(conn->sslconn,				\
+										nocontentresponse,			\
+										sizeof(nocontentresponse) - 1)
 #endif // HTTP_H
